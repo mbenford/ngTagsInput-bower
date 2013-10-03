f524cee2a3b62dde746306286c01be61055ad518
Michael Benford <michael@michaelbenford.net>
2013-10-03 01:46:13 -0300

    Updated to version 0.1.3

--- ng-tags-input.js
+++ ng-tags-input.js
@@ -1,33 +1,77 @@
 (function() {
 'use strict';
 
-angular.module('tags-input', []).directive('tagsInput', function() {
-    function toBool(value, defaultValue) {
-        return angular.isDefined(value) ? value === 'true' : defaultValue;
+/**
+ * @ngdoc directive
+ * @name tagsInput.directive:tagsInput
+ *
+ * @description
+ * ngTagsInput is an Angular directive that renders an input box with tag editing support.
+ *
+ * @param {string} ngModel Assignable angular expression to data-bind to.
+ * @param {string} ngClass CSS class to style the control.
+ * @param {number} tabindex Tab order of the control.
+ * @param {string=Add a tag} placeholder Placeholder text for the control.
+ * @param {number=3} minLength Minimum length for a new tag.
+ * @param {number=} maxLength Maximum length allowed for a new tag.
+ * @param {string=×} removeTagSymbol Symbol character for the remove tag button.
+ * @param {boolean=true} addOnEnter Flag indicating that a new tag will be added on pressing the ENTER key.
+ * @param {boolean=false} addOnSpace Flag indicating that a new tag will be added on pressing the SPACE key.
+ * @param {boolean=true} addOnComma Flag indicating that a new tag will be added on pressing the COMMA key.
+ * @param {boolean=true} replaceSpacesWithDashes Flag indicating that spaces will be replaced with dashes.
+ * @param {string=^[a-zA-Z0-9\s]+$*} allowedTagsPattern Regular expression that determines whether a new tag is valid.
+ * @param {boolean=false} enableEditingLastTag Flag indicating that the last tag will be moved back into
+ *                                             the new tag input box instead of being removed when the backspace key
+ *                                             is pressed and the input box is empty.
+ */
+
+angular.module('tags-input', []).directive('tagsInput', function($interpolate) {
+    function loadOptions(scope, attrs) {
+        function getStr(name, defaultValue) {
+            return attrs[name] ? $interpolate(attrs[name])(scope.$parent) : defaultValue;
+        }
+
+        function getInt(name, defaultValue) {
+            var value = getStr(name, null);
+            return value ? parseInt(value, 10) : defaultValue;
+        }
+
+        function getBool(name, defaultValue) {
+            var value = getStr(name, null);
+            return value ? value === 'true' : defaultValue;
+        }
+
+        scope.options = {
+            cssClass: getStr('ngClass', ''),
+            placeholder: getStr('placeholder', 'Add a tag'),
+            tabindex: getInt('tabindex', ''),
+            removeTagSymbol: getStr('removeTagSymbol', String.fromCharCode(215)),
+            replaceSpacesWithDashes: getBool('replaceSpacesWithDashes', true),
+            minLength: getInt('minLength', 3),
+            maxLength: getInt('maxLength', ''),
+            addOnEnter: getBool('addOnEnter', true),
+            addOnSpace: getBool('addOnSpace', false),
+            addOnComma: getBool('addOnComma', true),
+            allowedTagsPattern: new RegExp(getStr('allowedTagsPattern', '^[a-zA-Z0-9\\s]+$')),
+            enableEditingLastTag: getBool('enableEditingLastTag', false)
+        };
     }
 
     return {
         restrict: 'A,E',
-        scope: { tags: '=ngModel', cssClass: '@ngClass' },
+        scope: { tags: '=ngModel' },
         replace: false,
-        template: '<div class="ngTagsInput {{ cssClass }}">' +
+        template: '<div class="ngTagsInput {{ options.cssClass }}">' +
                   '  <ul>' +
-                  '    <li ng-repeat="tag in tags" ng-class="getCssClass($index)"><span>{{ tag }}</span><button type="button" ng-click="remove($index)">{{ removeTagSymbol }}</button></li>' +
+                  '    <li ng-repeat="tag in tags" ng-class="getCssClass($index)">' +
+                  '      <span>{{ tag }}</span>' +
+                  '      <button type="button" ng-click="remove($index)">{{ options.removeTagSymbol }}</button>' +
+                  '    </li>' +
                   '  </ul>' +
-                  '  <input type="text" placeholder="{{ placeholder }}" size="{{ placeholder.length }}" maxlength="{{ maxLength }}" tabindex="{{ tabindex }}" ng-model="newTag">' +
+                  '  <input type="text" placeholder="{{ options.placeholder }}" size="{{ options.placeholder.length }}" maxlength="{{ options.maxLength }}" tabindex="{{ options.tabindex }}" ng-model="newTag">' +
                   '</div>',
-        controller: ['$scope', '$attrs', function($scope, $attrs) {
-            $scope.placeholder = $attrs.placeholder || 'Add a tag';
-            $scope.tabindex= $attrs.tabindex;
-            $scope.removeTagSymbol = $attrs.removeTagSymbol || String.fromCharCode(215);
-            $scope.replaceSpacesWithDashes = toBool($attrs.replaceSpacesWithDashes, true);
-            $scope.minLength = $attrs.minLength || 3;
-            $scope.maxLength = Math.max($attrs.maxLength || $scope.placeholder.length, $scope.minLength);
-            $scope.addOnEnter = toBool($attrs.addOnEnter, true);
-            $scope.addOnSpace = toBool($attrs.addOnSpace, false);
-            $scope.addOnComma = toBool($attrs.addOnComma, true);
-            $scope.allowedTagsPattern = new RegExp($attrs.allowedTagsPattern || '^[a-zA-Z0-9\\s]+$');
-            $scope.enableEditingLastTag = toBool($attrs.enableEditingLastTag, false);
+        controller: function($scope, $attrs) {
+            loadOptions($scope, $attrs);
 
             $scope.newTag = '';
             $scope.tags = $scope.tags || [];
@@ -36,9 +80,9 @@ angular.module('tags-input', []).directive('tagsInput', function() {
                 var changed = false;
                 var tag = $scope.newTag;
 
-                if (tag.length >= $scope.minLength && $scope.allowedTagsPattern.test(tag)) {
+                if (tag.length >= $scope.options.minLength && $scope.options.allowedTagsPattern.test(tag)) {
 
-                    if ($scope.replaceSpacesWithDashes) {
+                    if ($scope.options.replaceSpacesWithDashes) {
                         tag = tag.replace(/\s/g, '-');
                     }
 
@@ -55,7 +99,7 @@ angular.module('tags-input', []).directive('tagsInput', function() {
             $scope.tryRemoveLast = function() {
                 var changed = false;
                 if ($scope.tags.length > 0) {
-                    if ($scope.enableEditingLastTag) {
+                    if ($scope.options.enableEditingLastTag) {
                         $scope.newTag = $scope.tags.pop();
                     }
                     else {
@@ -86,15 +130,15 @@ angular.module('tags-input', []).directive('tagsInput', function() {
                 $scope.shouldRemoveLastTag = false;
             });
 
-        }],
+        },
         link: function(scope, element) {
             var ENTER = 13, COMMA = 188, SPACE = 32, BACKSPACE = 8;
 
             element.find('input')
                 .bind('keydown', function(e) {
-                    if (e.keyCode === ENTER && scope.addOnEnter ||
-                        e.keyCode === COMMA && scope.addOnComma ||
-                        e.keyCode === SPACE && scope.addOnSpace) {
+                    if (e.keyCode === ENTER && scope.options.addOnEnter ||
+                        e.keyCode === COMMA && scope.options.addOnComma ||
+                        e.keyCode === SPACE && scope.options.addOnSpace) {
 
                         if (scope.tryAdd()) {
                             scope.$apply();
